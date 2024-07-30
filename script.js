79f146d3a42240a0d2b46530111405889073f1ae
tomek-m-coder <60541005+tomek-m-coder@users.noreply.github.com>
2024-07-30 22:10:39 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author ')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
 
 // let apiQuotes = [];
 
